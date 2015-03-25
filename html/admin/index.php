c4f9764bf30ff73e738295b55036855d0ced93e2
Rob Richey <robsnothome@gmail.com>
2015-03-25 11:47:11 -0600

    Changes form type for settings admin page.

--- html/admin/index.php
+++ html/admin/index.php
@@ -8,7 +8,7 @@
     <title>Config Pi Admin</title>
 
     <!-- Bootstrap -->
-    <link href="//css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,24 +19,36 @@
   </head>
   <body>
     <h1>Pi Settings</h1>
-    <form role="form">
-      <div class="form-group">
-        <label for="email">Email address:</label>
-        <input type="email" class="form-control" id="email">
+    <form class="form-horizontal" role="form">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" placeholder="Enter email">
       </div>
-      <div class="form-group">
-        <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd">
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-10"> 
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
       </div>
-      <div class="checkbox">
-        <label><input type="checkbox"> Remember me</label>
+    </div>
+    <div class="form-group"> 
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label><input type="checkbox"> Remember me</label>
+        </div>
       </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+    </div>
+    <div class="form-group"> 
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="//jquery/jquery.min.js"></script>
+    <script src="../jquery/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="//js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
