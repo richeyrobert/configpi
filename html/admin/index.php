ae793fa226afcaa9338b337d4e0e6c6d35176050
Rob Richey <robsnothome@gmail.com>
2015-03-25 11:54:06 -0600

    Debugging the form setup and actions.

--- html/admin/index.php
+++ html/admin/index.php
@@ -19,32 +19,26 @@
   </head>
   <body>
     <h1>Pi Settings</h1>
-    <form class="form-horizontal" role="form">
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="email">Email:</label>
-      <div class="col-sm-10">
-        <input type="email" class="form-control" id="email" placeholder="Enter email">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="pwd">Password:</label>
-      <div class="col-sm-10"> 
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-      </div>
-    </div>
-    <div class="form-group"> 
-      <div class="col-sm-offset-2 col-sm-10">
-        <div class="checkbox">
-          <label><input type="checkbox"> Remember me</label>
+    <form class="form-horizontal" role="form" method="post" action="index.php">
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="email">Email:</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="ip_address" name="ip_address" placeholder="192.168.1.20" value="<?php echo htmlspecialchars($_POST['ip_address']); ?>">
+          <?php echo "<p class='text-danger'>$errIPAddress</p>";?>
         </div>
       </div>
-    </div>
-    <div class="form-group"> 
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="pwd">Password:</label>
+        <div class="col-sm-10"> 
+          <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+        </div>
       </div>
-    </div>
-  </form>
+      <div class="form-group"> 
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">Submit</button>
+        </div>
+      </div>
+    </form>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="../jquery/jquery.min.js"></script>
